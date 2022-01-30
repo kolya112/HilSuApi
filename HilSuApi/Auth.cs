a4cf8c9919c66810b26c9ec7bd350194887b5083
kolya112 <nikolayurch@gmail.com>
2022-01-31 00:28:27 +0300

    Добавьте файлы проекта.

---
+++ HilSuApi/Auth.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace HilSuApi
+{
+    internal class Auth
+    {
+        // Конструктур с наличием токена
+        public Auth(string token)
+        {
+
+        }
+
+        // Конструктур без наличия токена
+        public Auth() { }
+
+        // Конструктур для авторизации через аккаунт
+        public Auth(string nickname, string password)
+        {
+
+        }
+    }
+}
---
+++ HilSuApi/HilSuHub.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace HilSuApi
+{
+    public class HilariousHub
+    {
+        protected string _baseURL = "https://api.hil.su/v2/";
+        protected string _baseURLv0 = "https://api.hil.su/v0/";
+        protected string _token;
+        protected string _user;
+        protected string _password;
+
+        // Конструктур с наличием токена
+        public HilariousHub(string token)
+        {
+            _token = token;
+        }
+
+        // Конструктур без наличия токена
+        public HilariousHub() { }
+
+        // Конструктур для авторизации через аккаунт
+        public HilariousHub(string nickname, string password)
+        {
+            _user = nickname;
+            _password = password;
+        }
+
+        public class Economy : HilariousHub
+        {
+            /// <summary>
+            /// Получить топ игроков по валюте
+            /// </summary>
+            /// <param name="limit"></param>
+            /// <param name="currency"></param>
+            public string TopPlayers(int limit, string currency = "coins")
+            {
+                HttpWebResponse request = Request("economy/top", $"limit={limit}&currency={currency}");
+                string answer = new StreamReader(request.GetResponseStream()).ReadToEnd();
+                return answer;
+            }
+        }
+
+        protected HttpWebResponse Request(string suburl, string parametrs)
+        {
+            return (HttpWebResponse)WebRequest.Create($"{_baseURL}{suburl}?{parametrs}").GetResponse();
+        }
+    }
+}
